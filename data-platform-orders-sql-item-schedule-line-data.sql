5fcf82f9a0e9206f9f0f35ad6f6a39fe9838ba8a
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-11-29 10:10:53 +0900

    release

--- data-platform-orders-sql-item-schedule-line-data.sql
+++ data-platform-orders-sql-item-schedule-line-data.sql
@@ -34,13 +34,10 @@ CREATE TABLE `data_platform_orders_item_schedule_line_data`
 
   PRIMARY KEY (`OrderID`, `OrderItem`, `ScheduleLine`),
     
-  CONSTRAINT `DataPlatformOrdersItemScheduleLineData_fk` FOREIGN KEY (`OrderID`, `OrderItem`) REFERENCES `data_platform_orders_item_data` (`OrderID`, `OrderItem`),
-  CONSTRAINT `DataPlatformOrdersItemScheduleLineDataProduct_fk` FOREIGN KEY (`Product`) REFERENCES `data_platform_product_master_general_data` (`Product`),
-  -- CONSTRAINT `DataPlatformOrdersItemScheduleLineDataStockConfirmationPlantTimeZone_fk` FOREIGN KEY (`StockConfirmationPlantTimeZone`) REFERENCES `data_platform_time_zone_time_zone_data` (`TimeZone`),
-  -- too long
-
-  -- CONSTRAINT `DataPlatformOrdersItemScheduleLineDataStockConfirmationPlantBatch_fk` FOREIGN KEY (`Product`, `StockConfirmationBussinessPartner`, `StockConfirmationPlant`, `StockConfirmationPlantBatch`) REFERENCES `data_platform_batch_master_record_batch_data` (`Product`, `BusinessPartner`, `Plant`, `Batch`)
-  -- Key column 'StockConfirmationBussinessPartner' doesn't exist in tableのエラー
+  CONSTRAINT `DPFMOrdersItemScheduleLineData_fk` FOREIGN KEY (`OrderID`, `OrderItem`) REFERENCES `data_platform_orders_item_data` (`OrderID`, `OrderItem`),
+  CONSTRAINT `DPFMOrdersItemScheduleLineDataProduct_fk` FOREIGN KEY (`Product`) REFERENCES `data_platform_product_master_general_data` (`Product`),
+  CONSTRAINT `DPFMOrdersItemScheduleLineDataSCPlantTimeZone_fk` FOREIGN KEY (`StockConfirmationPlantTimeZone`) REFERENCES `data_platform_time_zone_time_zone_data` (`TimeZone`),
+  CONSTRAINT `DPFMOrdersItemScheduleLineDataSCPlantBatch_fk` FOREIGN KEY (`Product`, `StockConfirmationBusinessPartner`, `StockConfirmationPlant`, `StockConfirmationPlantBatch`) REFERENCES `data_platform_batch_master_record_batch_data` (`Product`, `BusinessPartner`, `Plant`, `Batch`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
